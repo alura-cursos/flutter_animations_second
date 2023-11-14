8197dcaebcfec5209b5859dd563a0c3491dd5b90
Ricarth Lima <ricarth.lima@gmail.com>
2023-11-14 14:21:39 -0300

    v2.2 - tweenanimationbuilder

--- lib/screens/components/cover_dialog.dart
+++ lib/screens/components/cover_dialog.dart
@@ -1,37 +1,24 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 showCoverDialog({required BuildContext context, required String urlImage}) {
-  double scale = 0;
   showDialog(
     context: context,
     barrierColor: Colors.black.withAlpha(200),
     builder: (context) {
-      return StatefulBuilder(
-        builder: (context, setState) {
-          Timer timer = Timer(const Duration(milliseconds: 10), () {
-            setState(() {
-              scale = 1;
-            });
-          });
-          return Dialog(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            child: AnimatedScale(
-              scale: scale,
-              duration: const Duration(milliseconds: 500),
-              onEnd: () {
-                timer.cancel();
-              },
-              child: Image.network(
-                urlImage,
-                height: MediaQuery.of(context).size.height * 0.6,
-                fit: BoxFit.contain,
-              ),
-            ),
-          );
-        },
+      return Dialog(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        child: TweenAnimationBuilder<double>(
+          tween: Tween(begin: 0, end: 1),
+          duration: const Duration(milliseconds: 500),
+          builder: (context, value, child) {
+            return Image.network(
+              urlImage,
+              height: MediaQuery.of(context).size.height * 0.6,
+              fit: BoxFit.contain,
+            );
+          },
+        ),
       );
     },
   );
