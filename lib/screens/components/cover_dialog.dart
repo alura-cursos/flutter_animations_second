d61d68cb1c89c65e7952b037f71cbaef5b9feaec
Ricarth Lima <ricarth.lima@gmail.com>
2023-11-14 13:32:49 -0300

    v1.4 - ajustando dialog

--- lib/screens/components/cover_dialog.dart
+++ lib/screens/components/cover_dialog.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 showCoverDialog({required BuildContext context, required String urlImage}) {
   showDialog(
     context: context,
+    barrierColor: Colors.black.withAlpha(200),
     builder: (context) {
       return Dialog(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
         child: Image.network(
           urlImage,
+          height: MediaQuery.of(context).size.height * 0.6,
+          fit: BoxFit.contain,
         ),
       );
     },
