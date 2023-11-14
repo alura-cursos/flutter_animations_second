76803857ab85b8c76a55447c13aa724afdbb31be
Ricarth Lima <ricarth.lima@gmail.com>
2023-11-14 14:09:43 -0300

    v1.5 - animando dialog

--- lib/screens/components/cover_dialog.dart
+++ lib/screens/components/cover_dialog.dart
@@ -1,18 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 showCoverDialog({required BuildContext context, required String urlImage}) {
+  double scale = 0;
   showDialog(
     context: context,
     barrierColor: Colors.black.withAlpha(200),
     builder: (context) {
-      return Dialog(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        child: Image.network(
-          urlImage,
-          height: MediaQuery.of(context).size.height * 0.6,
-          fit: BoxFit.contain,
-        ),
+      return StatefulBuilder(
+        builder: (context, setState) {
+          Timer timer = Timer(const Duration(milliseconds: 10), () {
+            setState(() {
+              scale = 1;
+            });
+          });
+          return Dialog(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            child: AnimatedScale(
+              scale: scale,
+              duration: const Duration(milliseconds: 500),
+              onEnd: () {
+                timer.cancel();
+              },
+              child: Image.network(
+                urlImage,
+                height: MediaQuery.of(context).size.height * 0.6,
+                fit: BoxFit.contain,
+              ),
+            ),
+          );
+        },
       );
     },
   );
